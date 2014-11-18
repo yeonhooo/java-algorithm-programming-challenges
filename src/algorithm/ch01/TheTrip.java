8e108f6ed07a655309ce7a17e7f9e97702d818f5
yeonhooo <yeonhooo@gmail.com>
2014-11-19 01:49:51 +0900

    Add TheTrip.java file

---
+++ src/algorithm/ch01/TheTrip.java
@@ -0,0 +1,15 @@
+package algorithm.ch01;
+
+/**
+ * Programming Challenges 알고리즘 트레이닝 북
+ * Chap01. 여행(The Trip / page.42)
+ * 
+ * @author yeonhooo.lee
+ *
+ */
+public class TheTrip {
+	
+	public static void main(String[] args) {
+		
+	}
+}
